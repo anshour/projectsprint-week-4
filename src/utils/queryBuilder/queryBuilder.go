68f5b48183d4bd4b4d989d59e2af075eceb4d697
Anshor J <lordlexa10@gmail.com>
2024-05-25 09:40:28 +0700

    feat(chore) : add query builder utility

---
+++ src/utils/queryBuilder/queryBuilder.go
@@ -0,0 +1,33 @@
+package querybuilder
+
+import (
+	"strconv"
+)
+
+type Query struct {
+	BaseQuery string
+	Params    []interface{}
+}
+
+func (q *Query) nextParamIndex() string {
+	return "$" + strconv.Itoa(len(q.Params)+1)
+}
+
+func (q *Query) AppendCondition(column string, operator string, value interface{}) {
+	q.BaseQuery += " AND " + column + " " + operator + " " + q.nextParamIndex()
+	q.Params = append(q.Params, value)
+}
+
+func (q *Query) AppendOrder(column string, direction string) {
+	q.BaseQuery += " ORDER BY " + column + " " + direction
+}
+
+func (q *Query) AppendLimit(limit int) {
+	q.BaseQuery += " LIMIT " + q.nextParamIndex()
+	q.Params = append(q.Params, limit)
+}
+
+func (q *Query) AppendOffset(offset int) {
+	q.BaseQuery += " OFFSET " + q.nextParamIndex()
+	q.Params = append(q.Params, offset)
+}
