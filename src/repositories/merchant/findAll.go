5b478e2c27f4fe8584691ef4e9dfb3dc7dc14484
Anshor J <lordlexa10@gmail.com>
2024-05-25 09:40:50 +0700

    feat(merchant): refactor code to using query builder

--- src/repositories/merchant/findAll.go
+++ src/repositories/merchant/findAll.go
@@ -3,72 +3,40 @@ package merchantRepository
 import (
 	"log"
 	entity "projectsprintw4/src/entities"
-	"reflect"
-	"strconv"
-	"strings"
+	querybuilder "projectsprintw4/src/utils/queryBuilder"
 )
 
 func (r *sMerchantRepository) FindAll(filters *entity.MerchantListParams) (*[]entity.MerchantFindAllResult, error) {
-	baseQuery := "SELECT * FROM merchants WHERE true "
+	query := &querybuilder.Query{
+		BaseQuery: "SELECT * FROM merchants WHERE true",
+	}
 
-	params := []interface{}{}
+	if filters.MerchantId != "" {
+		query.AppendCondition("id", "=", filters.MerchantId)
+	}
 
-	n := (&entity.MerchantListParams{})
+	if filters.Name != "" {
+		query.AppendCondition("name", "ILIKE", "%"+filters.Name+"%")
+	}
 
-	if !reflect.DeepEqual(filters, n) {
-		conditions := []string{}
+	if filters.MerchantCategory != "" {
+		query.AppendCondition("category", "=", filters.MerchantCategory)
+	}
 
-		if filters.MerchantId != "" {
-			conditions = append(conditions, "id = $"+strconv.Itoa(len(params)+1))
-			params = append(params, filters.MerchantId)
-		}
-
-		if filters.Name != "" {
-			conditions = append(conditions, "name ILIKE $"+strconv.Itoa(len(params)+1))
-			params = append(params, "%"+filters.Name+"%")
-		}
-
-		if filters.MerchantCategory != "" {
-			conditions = append(conditions, "category = $"+strconv.Itoa(len(params)+1))
-			params = append(params, filters.MerchantCategory)
-		}
-
-		if len(conditions) > 0 {
-			baseQuery += " AND "
-			baseQuery += strings.Join(conditions, " AND ")
-		}
+	if filters.CreatedAt == "" || filters.CreatedAt == "desc" {
+		query.AppendOrder("created_at", "DESC")
+	} else if filters.CreatedAt == "asc" {
+		query.AppendOrder("created_at", "ASC")
 	}
 
 	if filters.Limit == 0 {
-		filters.Limit = 5
-	}
-
-	if filters.CreatedAt == "" {
-		filters.CreatedAt = "DESC"
-	}
-
-	if filters.CreatedAt == "asc" {
-		filters.CreatedAt = "ASC"
-	}
-
-	if filters.CreatedAt == "desc" {
-		filters.CreatedAt = "DESC"
-	}
-
-	baseQuery += " ORDER BY created_at " + filters.CreatedAt
-
-	baseQuery += " LIMIT $" + strconv.Itoa(len(params)+1)
-	params = append(params, filters.Limit)
-
-	if filters.Offset == 0 {
-		filters.Offset = 0
+		query.AppendLimit(5)
 	} else {
-		baseQuery += " OFFSET $" + strconv.Itoa(len(params)+1)
-		params = append(params, filters.Offset)
+		query.AppendLimit(filters.Limit)
 	}
 
 	merchants := make([]entity.MerchantFindAllResult, filters.Limit)
-	err := r.DB.Select(&merchants, baseQuery, params...)
+	err := r.DB.Select(&merchants, query.BaseQuery, query.Params...)
 	if err != nil {
 		log.Printf("Error finding merchants: %s", err)
 		return nil, err
