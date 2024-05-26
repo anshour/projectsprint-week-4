1489fe3eeef734cd49b9e6e3c058ec6a03e5ae29
Anshor J <lordlexa10@gmail.com>
2024-05-26 08:14:25 +0700

    feat(db): add some indexes

--- db/migrations/000100_create_index_table.up.sql
+++ db/migrations/000100_create_index_table.up.sql
@@ -1,1 +1,5 @@
 -- CREATE INDEX idx_users_nip ON users (nip);
+
+CREATE INDEX idx_merchants_on_created_at ON merchants(created_at);
+CREATE INDEX idx_merchant_items_on_created_at ON merchant_items(created_at);
+
