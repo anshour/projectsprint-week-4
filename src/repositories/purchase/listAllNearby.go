1e26dd23718062264ce4a921e34326db32e4dadb
Dion Fananie <dionfananie@gmail.com>
2024-05-31 06:24:08 +0700

    feat(purchase): update query

--- src/repositories/purchase/listAllNearby.go
+++ src/repositories/purchase/listAllNearby.go
@@ -4,49 +4,62 @@ import (
 	"fmt"
 	"log"
 	entity "projectsprintw4/src/entities"
-	querybuilder "projectsprintw4/src/utils/queryBuilder"
+	"strings"
 )
 
 func (r *sPurchaseRepository) ListAllNearby(filters *entity.ListNearbyParams) (*[]entity.ListNearbyMerchantResult, error) {
-	baseQuery := `SELECT 
-			merchant.id, merchant.name,
-			merchant.category, merchant.location_lat, 
-			merchant.location_long, merchant.image_url,
-			merchant.created_at,
-			item.name as item_name,  
-			item.category as item_category,
-			item.price as item_price, 
-			item.image_url as item_image_url,
-			item.created_at as item_created_at
-			FROM merchant_items item 
-			JOIN merchants merchant ON item.merchant_id = merchant.id
-			WHERE true`
+	baseQuery := `
+    SELECT 
+        merchant.id, merchant.name as merchant_name,
+        merchant.category as merchant_category, merchant.location_lat, 
+        merchant.location_long, merchant.image_url,
+        merchant.created_at as merchant_created_at,
+        item.name as item_name,  
+        item.category as item_category,
+        item.price as item_price, 
+        item.image_url as item_image_url,
+        item.created_at as item_created_at
+    FROM merchant_items item 
+    JOIN merchants merchant ON item.merchant_id = merchant.id
+    WHERE true`
 
-	query := &querybuilder.Query{
-		BaseQuery: baseQuery,
-	}
+	conditions := []string{}
+	args := []interface{}{}
+	argCounter := 1
 
 	if filters.MerchantId != "" {
-		query.AppendCondition("id", "=", filters.MerchantId)
+		conditions = append(conditions, fmt.Sprintf("merchant.id = $%d", argCounter))
+		args = append(args, filters.MerchantId)
+		argCounter++
 	}
 
 	if filters.Name != "" {
-		query.AppendCondition("name", "ILIKE", "%"+filters.Name+"%")
+		conditions = append(conditions, fmt.Sprintf("merchant.name ILIKE $%d", argCounter))
+		args = append(args, "%"+filters.Name+"%")
+		argCounter++
 	}
 
 	if filters.MerchantCategory != "" {
-		query.AppendCondition("category", "=", filters.MerchantCategory)
+		conditions = append(conditions, fmt.Sprintf("merchant.category = $%d", argCounter))
+		args = append(args, filters.MerchantCategory)
+		argCounter++
+	}
+
+	if len(conditions) > 0 {
+		baseQuery += " AND " + strings.Join(conditions, " AND ")
 	}
 
 	limit := filters.Limit
 	if limit == 0 {
 		limit = 5
 	}
-	// query.AppendLimit(limit)
+	baseQuery += fmt.Sprintf(" LIMIT $%d", argCounter)
+	args = append(args, limit)
 
 	// Print the generated SQL query and arguments
-	fmt.Printf("SQL Query: %s\n", query)
-	rows, err := r.DB.Queryx(query.BaseQuery, query.Params...)
+	fmt.Printf("SQL Query: %s\n", baseQuery)
+	fmt.Printf("Arguments: %v\n", args)
+	rows, err := r.DB.Queryx(baseQuery, args...)
 	if err != nil {
 		log.Printf("Error finding merchants nearby: %s", err)
 		return nil, err
