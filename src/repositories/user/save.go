4b38f230dae1fbbb7937ea4506347471468566bf
Anshor J <lordlexa10@gmail.com>
2024-06-11 09:22:53 +0700

    feat(user): fixing bug user id is not returned

--- src/repositories/user/save.go
+++ src/repositories/user/save.go
@@ -6,7 +6,7 @@ import (
 )
 
 func (r *sUserRepository) Save(data *entity.UserSaveParam) (*entity.UserSaveResult, error) {
-	query := "INSERT INTO users (username, email, password, role) VALUES ($1, $2, $3, $4) RETURNING username, password, email, role"
+	query := "INSERT INTO users (username, email, password, role) VALUES ($1, $2, $3, $4) RETURNING id, username, password, email, role"
 
 	var result entity.UserSaveResult
 	err := r.DB.QueryRowx(query, data.Username, data.Email, data.Password, data.Role).StructScan(&result)
