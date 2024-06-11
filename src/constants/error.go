b47139ba8b522eecb5ecd69bbbb854839f7492e9
Anshor J <lordlexa10@gmail.com>
2024-06-11 09:21:47 +0700

    feat(chore) : update some error constans

--- src/constants/error.go
+++ src/constants/error.go
@@ -13,12 +13,13 @@ var ErrWrongPassword = errors.New("wrong password verification")
 var ErrEmptyUserId = errors.New("Error Empty user id")
 var ErrUsernameAlreadyExist = errors.New("username or email already exist")
 var ErrMissingMerchantItem = errors.New("one of merchant or item not exist")
-var ErrTooFarLocation = errors.New("one of item has too far location")
+var ErrTooFarLocation = errors.New("One of item has too far location")
 var ErrNoRowsResultText = "sql: no rows in result set"
 var ErrErrorParsing = "Error parsing lat, long"
 var ErrInvalidType = "invalid type"
-var ErrStartingPoint = "need one starting point"
+var ErrStartingPoint = "Need one starting point"
 var ErrNoRowsResult = errors.New(ErrNoRowsResultText)
+var ErrNoLatLongPointProvided = errors.New("No latitude and longitude points provided")
 
 const (
 	UniqueViolationExistData    = pq.ErrorCode("23505") // 'unique_violation'
